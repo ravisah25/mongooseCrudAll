a0b983529c5aad6809271521d6cedc4977331471
unknown <rshankar.raja@gmail.com>
2019-01-29 05:17:43 +0530

    csd

---
+++ controllers/product.js
@@ -0,0 +1,82 @@
+var Product = require('../models/product');
+
+//Simple version, without validation or sanitation
+exports.test = function (req, res) {
+    res.send('Greetings from the Test controller!');
+};
+
+exports.product_create = function (req, res) {
+    var product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
+
+
+
+exports.product_csv = function (req, res) {
+    console.log(req);
+    if (!req.files)
+        return res.status(400).send('No files were uploaded.');
+
+    var authorFile = req.files.file;
+
+    var authors = [];
+
+    csv
+        .fromString(authorFile.data.toString(), {
+            headers: true,
+            ignoreEmpty: true
+        })
+        .on("data", function (data) {
+            data['_id'] = new mongoose.Types.ObjectId();
+
+            authors.push(data);
+        })
+        .on("end", function () {
+            product.create(authors, function (err, documents) {
+                if (err) throw err;
+
+                res.send(authors.length + ' authors have been successfully uploaded.');
+            });
+        });
+};
+
+exports.product_details = function (req, res) {
+    Product.findById(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    })
+};
+
+exports.product_details1 = function (req, res) {
+    Product.find(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    })
+};
+
+
+
+exports.product_update = function (req, res) {
+    Product.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err, product) {
+        if (err) return next(err);
+        res.send('Product udpated.');
+    });
+};
+
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
+};
